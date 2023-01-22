5c49bcba4341879e6a85471453f03b45506ac829
nina <nlina7731@gmail.com>
2023-01-21 21:09:46 -0800

    for loop example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numbers[]={1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int index = 0; index<numbers.length;index++){
+            sum += numbers[index];
+
+        }
+        System.out.println( sum);
+
+
     }
-}
+    }
